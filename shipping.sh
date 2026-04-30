54a609e0502120ee36f4ffca1616297dce7f11cf
raghu <raghumurukutla96@gmail.com>
2026-04-30 16:15:45 +0530

    adding shipping script

--- shipping.sh
+++ shipping.sh
@@ -23,7 +23,7 @@ fi
 
 VALIDATE $? "install mysql client in shipping client " &>> $LOG_FILE
 
-mysql -h mysql.devops26.sbs -uroot -p$rootpasswd -e 'use cities'  &>> $LOG_FILE
+mysql -h mysql.devops26.sbs -u root -p$rootpasswd -e 'use cities'  &>> $LOG_FILE
 
 if [ $? -ne 0 ]
 then
