3ddf1470d9fadb90c56b53a56149454dc8dcdcc3
raghu <raghumurukutla96@gmail.com>
2026-04-30 14:34:22 +0530

    adding catalogue script

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
-source ./common.sh
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
-check_root
+Start_time=$(date +%s)
+ 
+LOG_FOLDER="/var/log/roboshop-logs"
+mkdir -p $LOG_FOLDER
+LOG_FILENAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$LOG_FILENAME.log"
+
+current_directory=$PWD
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+   echo -e "$R please logg user as root user $W" &>> $LOG_FILE
+   exit 1
+else
+   echo -e "$G  logged  user as root user $W" &>> $LOG_FILE
+fi
+
+VALIDATE(){
+
+     if [ $1 -ne 0 ]
+   then 
+       echo -e "$R $2 is Failure:: $W" | tee -a $LOG_FILE
+       exit 1
+   else
+      echo -e "$G $2 is Success:: $W" | tee -a $LOG_FILE
+   fi
+}
 
 dnf module disable nginx -y &>> $LOG_FILE
 VALIDATE $? "disabling nginx module"
@@ -16,13 +45,16 @@ VALIDATE $? "install nginx "
 
 systemctl enable nginx &>> $LOG_FILE
 VALIDATE $? "enable nginx "
-systemctl start nginx 
+systemctl start nginx  &>> $LOG_FILE
 VALIDATE $? "start nginx "
 
 rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
+
 VALIDATE $? "removing default content "
 
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOG_FILE
+
 VALIDATE $? "downloading code "
 
 cd /usr/share/nginx/html 
