d612771687956afba17dc2034d9f01f431a35ef5
raghu <raghumurukutla96@gmail.com>
2026-04-30 17:20:36 +0530

    adding shipping script

--- mysql.sh
+++ mysql.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
  source ./common.sh
+ app_name="mysql"
  check_root
 
-dnf list --installed mysql-server &>> $LOG_FILE
-
-if [ $? -ne 0 ]
-then
-   echo -e "$R mysql not installed going to install  $W" &>> $LOG_FILE 
-   dnf install mysql-server -y &>> $LOG_FILE
-else
-   echo -e "$G mysql  installed in machine :: $Y skipping::  $W" &>> $LOG_FILE
-   
-fi
-
+echo -e "$G pleae enter mysql root psswd::$W"
+read -s rootpasswd
+echo -e "$G  mysql root psswd:: $rootpasswd $W"
+ 
+dnf install mysql-server -y &>> $LOG_FILE
 VALIDATE $? "mysql installation is"  &>> $LOG_FILE
 
 systemctl enable mysqld   &>> $LOG_FILE
@@ -20,10 +15,6 @@ VALIDATE $? "enable mysql "  &>> $LOG_FILE
 
 systemctl start mysqld  &>> $LOG_FILE
 VALIDATE $? "start mysql "  &>> $LOG_FILE
-
-echo -e "$G pleae enter mysql root psswd::$W"
-read -s rootpasswd
-echo -e "$G  mysql root psswd:: $rootpasswd $W"
 
 mysql_secure_installation --set-root-pass $rootpasswd
 
